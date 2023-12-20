1d0871cc88ef2e7792ca31b44f488ec2883fe279
kyongseo <102573192+kyounggseo@users.noreply.github.com>
2023-12-20 11:08:08 +0900

    유저 페이지 수정 구현 - 1

--- src/main/java/com/example/shoppingmall/domain/user/UserRepository.java
+++ src/main/java/com/example/shoppingmall/domain/user/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.shoppingmall.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
