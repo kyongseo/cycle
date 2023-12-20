59cfc058f6a892fe1b609ba16fa83a22a12ee9d4
kyongseo <102573192+kyounggseo@users.noreply.github.com>
2023-12-20 11:09:56 +0900

    판매량 순위 만들기

--- src/main/java/com/example/shoppingmall/web/SellerPageController.java
+++ src/main/java/com/example/shoppingmall/web/SellerPageController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @Controller
 public class SellerPageController {
